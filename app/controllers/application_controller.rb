7b08cf4ebfa45ddf009efd4d3f7d605ce50bf1d2
Petya Djanovska <petya.djanovska@gmail.com>
2018-05-23 23:02:41 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -35,7 +35,7 @@ class ApplicationController < Sinatra::Base
 
   delete '/posts/:id/delete' do
     @post = Post.find(params[:id])
-    @post.delete
+    @post.destroy
   end
 
   get '/posts/new' do
